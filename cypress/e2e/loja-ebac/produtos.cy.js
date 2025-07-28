2e3ee89bc3216779865f2a0c83fbe4916b2d8ddc
cleberdevfull <cleber.santos.almeida@gmail.com>
2025-07-28 20:33:20 -0300

    Massa de dados em lintas - Módulo 11

--- cypress/e2e/loja-ebac/produtos.cy.js
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -1,22 +1,37 @@
 /// <reference types="cypress"/>
-import { faker } from '@faker-js/faker'
+
+import produtosPage from "../../support/page-objects/produtos.page";
+
+//import { faker } from '@faker-js/faker'
+
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl('')
 
 });
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.last()
-            //.click()
-            //.eq(2)
-            .contains('Ariel Roll Sleeve Sweatshirt')
-            .click()
-
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
         cy.get('#tab-title-description > a').should('contain' , 'Descrição')
 
-    });
+        });
 
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        cy.get('.product_title').should('contain' , produto)
+        });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('abominable hoodie')
+        cy.get('.product_title').should('contain' , 'Abominable Hoodie')
+        });    
+    
+    it.only('Deve adicionar produto ao carrinho', () => {
+        let quantidade = 4
+        produtosPage.buscarProduto('abominable hoodie')
+        produtosPage.addProdutoCarrinho('L', 'Blue', quantidade )
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+        // cy.get('.product_title').should('contain' , '“Abominable Hoodie” foi adicionado no seu carrinho.')
+        });    
 })
